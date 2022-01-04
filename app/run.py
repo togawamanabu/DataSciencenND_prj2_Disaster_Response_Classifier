71d26c22860015db889d5ad1351680abd4fcca73
togawa manabu <togawamanabu@gmail.com>
2022-01-04 11:49:34 +0100

    added categories chart

--- app/run.py
+++ app/run.py
@@ -40,12 +40,14 @@ model = joblib.load("../models/classifier.pkl")
 def index():
     
     # extract data needed for visuals
-    # TODO: Below is an example - modify to extract data for your own visuals
     genre_counts = df.groupby('genre').count()['message']
-    genre_names = list(genre_counts.index)
+    genre_names = list(genre_counts.index) 
+
+    category_df = df.drop(['id', 'message', 'original', 'genre'], axis=1)
+    category_counts = category_df.sum()
+    category_names = list(category_df.columns)
     
     # create visuals
-    # TODO: Below is an example - modify to create your own visuals
     graphs = [
         {
             'data': [
@@ -64,6 +66,28 @@ def index():
                     'title': "Genre"
                 }
             }
+        },
+
+        {
+            'data': [
+                Bar(
+                    x=category_names,
+                    y=category_counts
+                )
+            ],
+
+            'layout': {
+                'title': 'Distribution of Message Categories',
+                'yaxis': {
+                    'title': "Count"
+                },
+                'xaxis': {
+                    'title': "Category"
+                },
+                'margin': {
+                    'b': 200
+                },
+            }       
         }
     ]
     
